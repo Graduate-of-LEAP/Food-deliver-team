e2d0fa35091aec6ad8974f1b724354c11f9d315d
Munkhgerelpurvee <164706050+Munkhgerelpurvee@users.noreply.github.com>
2024-10-08 21:40:58 +0800

    main page section1 -iin image bolon data hiiv.

--- frontend/src/app/components/Main.tsx
+++ frontend/src/app/components/Main.tsx
@@ -1,20 +1,54 @@
 "use client";
 import Image from "next/image";
+import { Sparkle } from "lucide-react";
+import { ChevronRight } from "lucide-react";
 
 const Main = () => {
   return (
     <>
       {" "}
-      Main page
-      <div className="flex bg-pink-200 "></div>
-      <Image
-        src="/images/food1.png"
-        alt="Description"
-        width={500}
-        height={500}
-        className=" flex
-          items-center justify-center mb-20 m-auto bg-green-200"
-      />
+      <div className="flex bg-[#18BA51] justify-around ">
+        <div
+          className="flex flex-col
+          items-center justify-center "
+        >
+          <h1 className="font-extrabold text-white border-b mb-3">
+            Pinecone Food delivery
+          </h1>
+          <p className="text-white font-thin">
+            Horem ipsum dolor sit amet,
+            <br /> consectetur adipiscing elit.
+          </p>
+        </div>
+        <div className="flex my-10">
+          <div className="relative">
+            <Image
+              src="/images/main1.png"
+              alt="Description"
+              width={343}
+              height={338}
+              className=""
+            />
+          </div>
+        </div>
+      </div>
+      {/*  */}
+      <div className="flex justify-between mx-28">
+        <div className="flex font-bold ">
+          <Sparkle className="text-green-400" />
+          <Image
+            src="/images/Star.png"
+            alt="Description"
+            width={20}
+            height={20}
+            className=""
+          />
+           Хямдралтай
+        </div>
+        <div className="flex text-[#18BA51] font-normal">
+          Бүгдийг харах <ChevronRight />
+        </div>
+      </div>
     </>
   );
 };
