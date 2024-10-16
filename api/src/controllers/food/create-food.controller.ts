56fae16c9905cde98f4c898a97bfa47c65f8af99
Munkhgerelpurvee <164706050+Munkhgerelpurvee@users.noreply.github.com>
2024-10-16 20:12:16 +0800

    create-food-controller

---
+++ api/src/controllers/food/create-food.controller.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from "express";
+import { foodModel } from "../../models/food.schema";
+
+export const createFoodController: RequestHandler = async (req, res) => {
+  try {
+    await foodModel.create({
+      ...req.body,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    return res.status(201).json({
+      message: "Product nemegdlee",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Product buruu l nemeed bndaa",
+    });
+  }
+};
