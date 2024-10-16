4b7985ea15fa7a68c09abce74c9a65e128452042
24LP4153 24LP4153 <24LP4153@G301-CC0195.local>
2024-10-16 11:22:22 +0800

    pop-up_product

--- frontend/src/app/(main)/components/FoodSweet.tsx
+++ frontend/src/app/(main)/components/FoodSweet.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import Image from "next/image";
-import Link from "next/link";
+import { useState } from "react";
 import { Sparkle } from "lucide-react";
 import { ChevronRight } from "lucide-react";
 
@@ -29,6 +29,15 @@ const slidesFood = [
 ];
 
 export const FoodSweet = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleDecrease = () => {
+    setQuantity((prevQuantity) => Math.max(prevQuantity - 1, 1));
+  };
+
+  const handleIncrease = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
   return (
     <>
       <div className="flex flex-col">
@@ -42,7 +51,7 @@ export const FoodSweet = () => {
               height={20}
               className=""
             />
-             Амттан
+            Амттан
           </div>
           <div className="flex text-[#18BA51] font-normal">
             Бүгдийг харах <ChevronRight />
@@ -51,14 +60,70 @@ export const FoodSweet = () => {
         <div className=" grid grid-cols-4 grid-flow-row gap-5 my-10">
           {slidesFood?.map((item, index) => {
             return (
-              <div key={index}>
-                <FoodSweetCard
-                  key={index}
-                  src={item.src}
-                  title={item.title}
-                  price={item.price}
-                />
-              </div>
+              <Dialog key={index}>
+                <DialogTrigger asChild>
+                  <div
+                    className="cursor-pointer"
+                    onClick={() => { }}
+                  >
+
+                    <FoodSweetCard
+                      key={index}
+                      src={item.src}
+                      title={item.title}
+                      price={item.price}
+                    />
+
+                  </div>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[800px] flex gap-8">
+                  <div className="w-[48%]">
+                    <Image
+                      src={item.src}
+                      width={800}
+                      height={800}
+                      alt="Picture of the pizza"
+                      className="h-full w-full object-cover rounded-2xl"
+                    />
+                  </div>
+
+                  <div className="w-[48%] flex flex-col py-8">
+                    <div>
+                      <b className="text-2xl">{item.title}</b>
+                      <p className="text-green-500 text-lg font-bold py-4">{item.price} ₮</p>
+                    </div>
+                    <div>
+                      <b className="text-lg">Орц</b>
+                      <p className="p-2 bg-gray-50 rounded-lg my-2">
+                        Хулуу, төмс, лууван, сонгино, цөцгийн тос, самрын үр
+                      </p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                      <b className="text-lg">Тоо</b>
+                      <div>
+                        <div className="flex justify-between">
+                          <button
+                            className="h-10 px-4 text-xl rounded-lg bg-green-500 text-white"
+                            onClick={handleDecrease}>
+                            -
+                          </button>
+                          <div className="flex items-center">{quantity}</div>
+                          <button
+                            className="h-10 px-4 text-xl rounded-lg bg-green-500 text-white"
+                            onClick={handleIncrease}
+                          >
+                            +
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <button className="mt-8 h-12 rounded-sm px-20 bg-green-500 flex justify-center text-white items-center">
+                      Сагслах
+                    </button>
+                  </div>
+                </DialogContent>
+              </Dialog>
+
             );
           })}
         </div>
@@ -72,7 +137,6 @@ type foodCardType = {
   title: string;
   price: number;
 };
-
 export const FoodSweetCard = ({ src, title, price }: foodCardType) => {
   return (
     <div className="">
--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,13 +1,11 @@
 import { Cart } from "./components/Cart";
 import Main from "./components/Main";
-import { Order } from "./components/Order";
+
 
 export default function Home() {
   return (
     <>
       <Main />
-      <Order />
-      {/* <Cart /> */}
     </>
   );
 }
